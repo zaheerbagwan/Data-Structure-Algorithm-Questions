8205a644dce69aa2ed158cdd328ea9427108b76d
Zaheer Bagwan <153502440+zaheerbagwan@users.noreply.github.com>
2025-05-15 14:56:15 +0530

    Add files via upload

---
+++ Maths Questions/ArmstrongNumber2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class ArmstrongNumber2
+{
+	public void Checker(int num)
+	{
+		int digits=0;
+		int copy=num;
+		int result=0;
+		while(num>0)
+		{
+			digits=num%10;
+			result=(int) Math.pow(digits, 3);
+			num=num/10;
+
+		}
+	
+		System.out.println(copy==result);
+	}
+
+	public static void main(String []args)	{
+	
+	ArmstrongNumber an = new ArmstrongNumber();
+
+	Scanner s = new Scanner(System.in);
+	int num=s.nextInt();
+
+	an.Checker(num);
+	}
+}
