4691cf7a9ccda7bef2b3d9ed7a6e2a4fdf7d4d2b
Zaheer Bagwan <153502440+zaheerbagwan@users.noreply.github.com>
2025-06-05 17:17:49 +0530

    adding comment in binarySearchOccurence.java

--- BinarySearch/binarySearchOccurence.java
+++ BinarySearch/binarySearchOccurence.java
@@ -1,60 +1,59 @@
 class binarySearchOccurence
 {
-	public static int  Occurence(int arr[], int target)
+	public static int Occurence(int arr[], int target)
 	{
-		int firstIndex = binarySearch(arr, target, true);
-		int lastIndex = binarySearch(arr, target, false);
+		int firstIndex = binarySearch(arr, target, true);	// Find the first occurrence of the target
+		int lastIndex = binarySearch(arr, target, false);	// Find the last occurrence of the target
 
-		if(firstIndex==-1)
+		if(firstIndex == -1)	// If target not found, return 0
 		{
 			return 0;
 		}
-		int occurence=lastIndex-firstIndex+1;
-		
-		
-		return occurence;
+		int occurence = lastIndex - firstIndex + 1;	// Calculate number of occurrences
+
+		return occurence;	// Return the total occurrences
 	}
 
 	public static int binarySearch(int arr[], int target, boolean isFirst)
 	{
-		int start=0;
-		int end=arr.length-1;
-		int ans=-1;
-		int mid=0;
+		int start = 0;
+		int end = arr.length - 1;
+		int ans = -1;	// Default answer if target not found
+		int mid = 0;
 
-		while(start<=end)
+		while(start <= end)	// Standard binary search loop
 		{
-			mid=start+(end-start)/2;
-			if(target==arr[mid])
+			mid = start + (end - start) / 2;	// Calculate the mid index
+
+			if(target == arr[mid])	// If mid element is target
 			{
-				ans=mid;
-				if(isFirst)
+				ans = mid;	// Store current index as potential answer
+				if(isFirst)	// If looking for first occurrence
 				{
-					end=mid-1;
+					end = mid - 1;	// Search left half
 				}
-				else
+				else	// If looking for last occurrence
 				{
-					start=mid+1;
+					start = mid + 1;	// Search right half
 				}
 			}
-			else if(target>arr[mid])
+			else if(target > arr[mid])	// If target is greater, go right
 			{
-				start=mid+1;
+				start = mid + 1;
 			}
-			else
+			else	// If target is smaller, go left
 			{
-				end=mid-1;
+				end = mid - 1;
 			}
 		}
 
-		return ans;
+		return ans;	// Return the index of first/last occurrence or -1
 	}
 
-	public static void main(String []args)		{
-
-	int arr[]={10,20,30,40,50,50,50,60};
-	int target=50;
-	System.out.println(Occurence(arr, target));
+	public static void main(String []args)		
+	{
+		int arr[] = {10, 20, 30, 40, 50, 50, 50, 60};
+		int target = 50;
+		System.out.println(Occurence(arr, target));
 	}
 }
-		
